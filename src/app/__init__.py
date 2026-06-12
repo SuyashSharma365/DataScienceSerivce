3c4b596942d0459b01ca9d75101b92f1a76af793
Suyash Sharma <sharmasuyash198@gmail.com>
2026-06-12 22:42:12 +0530

    Added kafka producer to send message to expense service

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,21 +1,27 @@
 from flask import Flask
 from flask import request , jsonify
 from service.messageService import MessageService
+from kafka import KafkaProducer
+import json
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messageService = MessageService()
+producer = KafkaProducer(bootstrap_servers=app.config['KAFKA_BOOTSTRAP_SERVERS'],
+                             value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
 @app.route("/v1/ds/message" , methods=["POST"])
 def handleMessage():
     message = request.json.get("message")
     result = messageService.processMessage(message)
+    serialized_result = result.model_dump()
+    producer.send(app.config['KAFKA_TOPIC'], value=serialized_result)
     return jsonify(result.model_dump())
 
 @app.route("/" , methods=["GET"])
 def home():
-    print("Hello World")
+    return jsonify({"message": "Hello World"})
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
